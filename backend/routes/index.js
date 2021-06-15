cfca017ad00d4f6f5f59e9a12a488de0eb1f73e1
Pedro Conde <condepedro968@gmail.com>
2021-06-15 19:25:06 +0200

    move back to other repo

--- backend/routes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-
-    // Base URLS
-    app.use('/', require('./base.routes.js'))
-    app.use('/api/auth', require('./auth.routes.js'))
-    app.use('/api', require('./trading.routes.js'))
-}
