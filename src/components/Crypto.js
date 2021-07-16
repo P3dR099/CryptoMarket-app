89e7ebecd6a584c5e38bc9aed413621738bf6a4e
Pedro Conde <condepedro968@gmail.com>
2021-07-16 16:23:12 +0200

    adding changes

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -160,7 +160,7 @@ const CardCrypto = (props) => {
         if (matchesDown) {
             return (
                 <Grid container spacing={3} style={{ display: 'flex', whiteSpace: 'pre', justifyContent: 'flex-end', flexBasis: '155%' }}>
-                    <span className={coinInfo.CHANGEPCT24HOUR < 0 ? classBottom.red : classBottom.green} style={{ width: 40, height: 12, fontSize: 9, marginTop: !matches ? 16.5 : 19, placeItems: 'center', padding: '3px 11px 4px 3px', marginLeft: 15, marginRight: 2 }}>
+                    <span className={coinInfo.CHANGEPCT24HOUR < 0 ? classBottom.red : classBottom.green} style={{ width: 40, height: 12, fontSize: 9, marginTop: !matches ? 15.5 : 19, placeItems: 'center', padding: '3px 11px 4px 3px', marginLeft: 15, marginRight: 2 }}>
                         {coinInfo.CHANGEPCT24HOUR > 0 ? <ArrowDropUpIcon style={{ width: 26 }} /> : <ArrowDropDownIcon style={{ width: 26 }} />}
                         {coinInfo.CHANGEPCT24HOUR !== undefined && coinInfo.CHANGEPCT24HOUR.toFixed(2)}
                     </span>
